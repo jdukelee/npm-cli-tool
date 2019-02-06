96e289ae05e9ad4c936ef8837ca0a532f487c1ce
Jon Lee <jdukelee@gmail.com>
2019-02-06 10:46:01 -0800

    Add sample cli

---
+++ bin/cli.js
@@ -0,0 +1,5 @@
+#! /usr/bin/env node
+var shell = require('shelljs');
+
+shell.exec('echo You can do stuff here in node!');
+shell.exec('echo Like using shelljs to do things like install other modules or read/write files.');
